8284fde8881171918bd022f49df3ab5f3131a093
Yuuki883 <yuukisaitou14@gmail.com>
2022-04-24 02:35:12 +0900

    "commit"

--- 5章 コンテナ/contena.py
+++ 5章 コンテナ/contena.py
@@ -43,3 +43,70 @@ colors = ["blue","green","yellow"]
 colors
 colors[2] = "red"
 colors
+
+#pop=リストの末尾から要素削除
+colors = ["blue","green","yellow"]
+colors
+item = colors.pop()
+item
+colors
+
+#リスト結合
+colors1 = ["red","green","yellow"]
+colors2 = ["orange","green","yellow"]
+colors1 + colors2
+
+#in＝リストに含まれている要素を確認
+colors = ["blue","green","yellow"]
+"green" in colors
+
+#in＝リストに含まれていない要素を確認
+colors = ["blue", "green", "yellow"]
+"black" not in colors
+
+#コンテナ内の要素数確認
+len(colors)
+
+#3種類の色当て
+colors = ["purple","orange","green"]
+guess = input("何色でしょう(入力してください):")
+
+if guess in colors:
+  print("当たり!")
+else:
+  print("ハズレ!また挑戦してね。")
+
+#タプル=一度作ると内容を変更できない.変更してほしくない値で使用
+my_tuple = tuple()
+my_tuple
+my_tuple = ()
+my_tuple
+
+#タプルにオブジェクトを追加する場合
+rndm = ("M. jackson",1958,True)
+rndm
+
+#要素が一つの場合は,必要
+#コレはタプルです
+('self_taught',)
+
+#コレはタプルではなく数値演算に使うカッコ
+(9)+1
+
+#コレはタプルではなく数値演算に使うカッコ
+(9)+1
+
+#要素変更不可
+dys = ("M. jackson", 1958, True)
+dys[1] = "Handomaids Tale"
+
+#要素を取り出す場合にはインデックス
+dys = ("M. jackson", 1958, True)
+dys[0]
+
+#要素の確認はin、入っていないことの確認はnotin
+dys = ("M. jackson", 1958, True)
+"M. jackson" in dys
+
+dys = ("M. jackson", 1958, True)
+"Handomaids Tale" not in dys
