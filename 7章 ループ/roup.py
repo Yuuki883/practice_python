6fdd4788e2afe6e43c60bca1b320722c60675961
Yuuki883 <yuukisaitou14@gmail.com>
2022-05-01 18:56:16 +0900

    "ループ"

---
+++ 7章 ループ/roup.py
@@ -0,0 +1,15 @@
+#for [変数名] in [イテラブル]:
+    #[コードブロック]
+
+name = "Ted"
+for character in name:
+  print (character)
+
+shows = ["Got","Narcos","Vice"]
+for show in shows:
+  print (show)
+  
+#tuple
+coms = ("A.Development","Friends","Always Sunny")
+for show in coms:
+  print (show)
